58fb5bb5a2a1edd74e93754fe24c46f1473ccef2
Jonathan Waldorf <jonathanwaldorf05@gmail.com>
2025-09-11 20:43:56 -0400

    Initial release-ready snapshot (cleaned by John)

---
+++ src/quera_animation_toolbox/__init__.py
@@ -0,0 +1,7 @@
+"""
+Make the “quera_animation_toolbox” folder importable.
+
+Exposes the most common symbols at the top level so users can write
+
+    from quera_animation_toolbox import quera_colors, quera_qubit_lib
+"""
